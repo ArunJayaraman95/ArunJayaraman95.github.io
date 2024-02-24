2be87e9cfda41326e17e2b5d4ddbb1f9404f11d2
Arun Jayaraman <riparun.aj@gmail.com>
2024-02-24 18:44:13 -0500

    Anilist Query Works
    JS adds a card for each anime in favorites list

--- animes.js
+++ animes.js
@@ -1,48 +1,108 @@
-const continentSelect = document.getElementById("continent-select");
-
-queryFetch(`
-    query {
-        continents {
-            name
-            code
-        }
-    }`).then((data) => {
-  console.log(data.data.continents);
-  data.data.continents.forEach((continent) => {
-    const option = document.createElement("option");
-    option.value = continent.code;
-    option.innerText = continent.name;
-    continentSelect.append(option);
-  });
-});
-
-continentSelect.addEventListener("change", (e) => {
-  const continentCode = e.target.value;
-  const countries = getContinentCountries(continentCode);
-});
-
-function getContinentCountries(continentCode) {
-    return queryFetch(`
-      query getCountries($code: String) {
-        continent(code: $code) {
-          countries {
-            name
+// Here we define our query as a multi-line string
+// Storing it in a separate .graphql/.gql file is also possible
+var query = `
+query {
+    Page {
+      users(name:"SpiderJaw") {
+        name
+        id
+        favourites {
+          anime {
+            nodes {
+              id
+              title {
+                english
+              }
+              coverImage {
+                extraLarge
+              }
+            }
           }
         }
       }
-    `, { code: continentCode }).then(data => {
-      return data.data.continent.countries
-    })
+    }
   }
   
+`;
 
-function queryFetch(query, variables) {
-  return fetch('https://countries.trevorblades.com/', {
-    method: 'POST',
-    headers: { "Content-Type": "application/json" },
+// Define our query variables and values that will be used in the query request
+var variables = {
+  id: 1,
+};
+
+// Define the config we'll need for our Api request
+var url = "https://graphql.anilist.co",
+  options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
     body: JSON.stringify({
       query: query,
-      variables: variables
-    })
-  }).then(res => res.json())
+      variables: variables,
+    }),
+  };
+
+// Make the HTTP Api request
+fetch(url, options).then(handleResponse).then(handleData).catch(handleError);
+
+function handleResponse(response) {
+  return response.json().then(function (json) {
+    return response.ok ? json : Promise.reject(json);
+  });
+}
+
+const continentSelect = document.getElementById("continent-select");
+const countryList = document.getElementById("countries-list");
+const usernameTitle = document.getElementById("TitleBoi");
+
+function handleData(data) {
+  var siteUserName = data.data.Page.users[0].name;
+  var animeList = data.data.Page.users[0].favourites.anime.nodes;
+  usernameTitle.innerHTML = siteUserName;
+
+  animeList.forEach((anime) => {
+    var title = anime.title.english;
+    var imageSrc = anime.coverImage.extraLarge;
+
+    console.log(title);
+    console.log(imageSrc);
+    createCard(title, imageSrc);
+  });
+
+  console.log(data.data.Page);
+  console.log(data.data.Page.users[0].favourites.anime.nodes);
+}
+
+function handleError(error) {
+  alert("Error, check console");
+  console.error(error);
+}
+
+function createCard(text, imageUrl) {
+  // Create card container
+  var cardContainer = document.getElementById("cardContainer");
+
+  // Create card element
+  var card = document.createElement("div");
+  card.classList.add("card");
+
+  // Create image element
+  var img = document.createElement("img");
+  img.src = imageUrl;
+  img.classList.add("card-image");
+
+  // Create h2 element
+  var heading = document.createElement("h2");
+  heading.textContent = text;
+  heading.classList.add("card-text");
+
+  // Append elements to card
+  card.appendChild(img);
+  card.appendChild(document.createElement("br")); // Line break
+  card.appendChild(heading);
+
+  // Append card to container
+  cardContainer.appendChild(card);
 }
